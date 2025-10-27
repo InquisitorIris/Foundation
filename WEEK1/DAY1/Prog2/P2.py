c22b26be1e4487741269e964b06f90eeed4b1d72
Anushka Raghunandan <inquisitoriris@outlook.com>
2025-10-27 13:19:59 +0530

    Create P2.py

---
+++ WEEK1/DAY1/Prog2/P2.py
@@ -0,0 +1,23 @@
+print("INTEGER SUM")
+a=2
+b=3
+print("a = ",a)
+print("b = ",b)
+print("a+b = ",a+b)
+
+c=int(input("Enter c: "))
+d=int(input("Enter d: "))
+print("c+d = ",c+d)
+
+print("***********************")
+
+print("DECIMAL SUM")
+a=2.1
+b=3.4
+print("a = ",a)
+print("b = ",b)
+print("a+b = ",a+b)
+
+c=float(input("Enter c: "))
+d=float(input("Enter d: "))
+print("c+d = ",c+d)
